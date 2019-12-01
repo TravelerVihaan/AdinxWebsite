cdb78a7ad81eb3bb930af2b7cd52b88dcc2f3f7c
Vihaan <lukasz.biel@10g.pl>
2019-12-01 21:49:13 +0100

    First commit

---
+++ src/main/java/com/github/vihaan/adinxwebsite/AdinxWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.vihaan.adinxwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdinxWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdinxWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/vihaan/adinxwebsite/AdinxWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vihaan.adinxwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdinxWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

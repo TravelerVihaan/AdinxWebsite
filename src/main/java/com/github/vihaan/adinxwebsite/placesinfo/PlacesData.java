317297429edc95d15ad53cd7e55eaa26447a6a20
Vihaan <lukasz.biel@10g.pl>
2019-12-22 13:14:58 +0100

    HotelsData

---
+++ src/main/java/com/github/vihaan/adinxwebsite/placesinfo/PlacesData.java
@@ -0,0 +1,10 @@
+package com.github.vihaan.adinxwebsite.placesinfo;
+
+public class PlacesData {
+
+    public final static String aHotelUrl = "example.com";
+
+    public final static String aHotelLogin = "example";
+
+    public final static String aHotelp = "example";
+}

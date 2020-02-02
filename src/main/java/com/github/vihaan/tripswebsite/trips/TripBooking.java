87fb50faf3b7ae016fe1c33f33f3acf1b16110a5
Vihaan <lukasz.biel@10g.pl>
2020-02-02 02:06:37 +0100

    TripBooking service

---
+++ src/main/java/com/github/vihaan/tripswebsite/trips/TripBooking.java
@@ -0,0 +1,41 @@
+package com.github.vihaan.tripswebsite.trips;
+
+import com.github.vihaan.tripswebsite.mappers.IMapper;
+import com.github.vihaan.tripswebsite.validation.IValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TripBooking {
+
+    private TripRepository tripRepository;
+    private IMapper<Trip, TripDTO> tripMapper;
+    private IValidation<TripDTO> tripValidator;
+    private DestinationRepository destinationRepository;
+
+    @Autowired
+    public TripBooking(@Qualifier("tripMapper") IMapper tripMapper,
+                       @Qualifier("tripValidation") IValidation tripValidator,
+                       DestinationRepository destinationRepository,
+                       TripRepository tripRepository){
+        this.tripMapper = tripMapper;
+        this.tripValidator = tripValidator;
+        this.tripRepository = tripRepository;
+        this.destinationRepository = destinationRepository;
+    }
+
+    public void executeBooking(TripDTO tripDTO){
+        if(!tripValidator.isValid(tripDTO))
+            return; //TODO
+        Trip trip = tripMapper.convertDtoToEntity(tripDTO);
+        trip.setTripDestination(prepareDestinationEntityToSave(tripDTO));
+        tripRepository.save(trip);
+
+
+    }
+
+    private Destination prepareDestinationEntityToSave(TripDTO tripDTO){
+        return destinationRepository.findByDestination(tripDTO.getDestination().getDestination()).orElseThrow();
+    }
+}

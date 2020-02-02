c9a2c41d02d33fcdc6023deb1b5c966bd3cf989f
Vihaan <lukasz.biel@10g.pl>
2020-02-02 01:45:07 +0100

    New User Service

---
+++ src/main/java/com/github/vihaan/tripswebsite/users/AddNewUser.java
@@ -0,0 +1,45 @@
+package com.github.vihaan.tripswebsite.users;
+
+import com.github.vihaan.tripswebsite.mappers.IMapper;
+import com.github.vihaan.tripswebsite.validation.IValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class AddNewUser {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private IMapper<User, UserDTO> userMapper;
+    private IValidation<UserDTO> userValidator;
+
+    @Autowired
+    public AddNewUser(@Qualifier("userMapper") IMapper userMapper,
+                      @Qualifier("userValidation") IValidation userValidator,
+                      UserRepository userRepository,
+                      RoleRepository roleRepository){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.userMapper = userMapper;
+        this.userValidator = userValidator;
+    }
+
+    public void addNewUser(UserDTO userDTO){
+        if(!userValidator.isValid(userDTO)){
+            return; //TODO
+        }
+        User user = userMapper.convertDtoToEntity(userDTO);
+        Set<RoleDTO> rolesDtoSet = userDTO.getRoles();
+        Set<Role> roles = rolesDtoSet.stream().map(role -> findAndConvertToRole(role.getRole())).collect(Collectors.toSet());
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+
+    private Role findAndConvertToRole(String role){
+        return roleRepository.findByRole(role).orElseThrow();
+    }
+}

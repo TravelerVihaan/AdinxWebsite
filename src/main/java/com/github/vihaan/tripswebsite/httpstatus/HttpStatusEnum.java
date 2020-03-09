52db4f8fa89830284ea92d47f199072acf5c6de5
Vihaan <lukasz.biel@10g.pl>
2020-03-09 20:34:52 +0100

    HttpStatusEnum

---
+++ src/main/java/com/github/vihaan/tripswebsite/httpstatus/HttpStatusEnum.java
@@ -0,0 +1,22 @@
+package com.github.vihaan.tripswebsite.httpstatus;
+
+import org.springframework.http.HttpStatus;
+
+public enum HttpStatusEnum {
+    OK(HttpStatus.OK),
+    CONFLICT(HttpStatus.CONFLICT),
+    BADREQUEST(HttpStatus.BAD_REQUEST),
+    NOTFOUND(HttpStatus.NOT_FOUND),
+    CREATED(HttpStatus.CREATED);
+
+    HttpStatus response;
+
+    HttpStatusEnum(HttpStatus response){
+        this.response = response;
+    }
+
+    HttpStatus getResponse(){
+        return response;
+    }
+
+}
